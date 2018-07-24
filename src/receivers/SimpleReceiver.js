74e9d3868fe8d3ee0f0084c19e4ef4869f47d56a
Dexter Watkins <dexter.a.watkins@vanderbilt.edu>
2018-07-24 12:56:39 -0500

    simple frameInfo stream receiver

---
+++ src/receivers/SimpleReceiver.js
@@ -0,0 +1,28 @@
+/**
+* Simple stream receiver to print incoming 'frameInfo' datapackets
+**/
+"use strict"
+var log = console.log.bind(console);
+const minimist = require('minimist');
+
+var args = minimist(process.argv.slice(2),{
+    string: 'ip',
+    default: {
+        ip: '127.0.0.1',
+        port: 1337
+    }
+})
+
+var socket = require('socket.io-client')('http://' + args.ip + ':' + args.port);
+
+socket.on("connect", function() {
+    log('Connected!')
+
+    socket.on('frameInfo', data => {
+        log(data)
+    })
+
+    socket.on("disconnect", function(){
+        log('Disconnected!')
+    })
+})
